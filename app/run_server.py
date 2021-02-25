7be94fba34d866f891083e8e42ce8bc6932a6318
Haegdorens, Gert <gert.haegdorens@yrcw.com>
2021-02-25 07:18:37 -0600

    Initial code for run_server.py

--- app/run_server.py
+++ app/run_server.py
@@ -1,7 +1,19 @@
-import sys
+from zeppos_api.run_server_base import RunServerBase
+from zeppos_logging.app_logger import AppLogger
 
-def main(args):
-    pass
+
+class RunServer(RunServerBase):
+    def __init__(self):
+        super().__init__(current_module_filename=__file__)
+
+    def run_server(self):
+        if super().start_app():
+            pass
+
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    RunServerBase.main_basic_logging(logger_name="youtube_downloader",
+                                     current_module_filename=__file__)
+    AppLogger.logger.debug("Entering __main__")
+    RunServer().run_server()
+    # main(sys.argv[1:])
