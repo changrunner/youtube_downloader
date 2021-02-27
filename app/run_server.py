e8ed70e50a35801e8742b846165578276118043f
changrunner <gert@changrunner.com>
2021-02-27 06:51:42 -0600

    Add run_server.py in the app folder

--- app/run_server.py
+++ app/run_server.py
@@ -1,5 +1,9 @@
 from zeppos_api.run_server_base import RunServerBase
 from zeppos_logging.app_logger import AppLogger
+from flask import Flask
+from flask_restful import Api
+from waitress import serve
+from paste.translogger import TransLogger
 
 
 class RunServer(RunServerBase):
@@ -8,7 +12,26 @@ class RunServer(RunServerBase):
 
     def run_server(self):
         if super().start_app():
-            pass
+            LISTEN = f"127.0.0.1:{8080}"
+            serve(TransLogger(application=App.create_app_instance([HeartBeat]),
+                              logger=AppLogger.logger,
+                              setup_console_handler=True)
+                  , listen=LISTEN)
+
+class App:
+    def create_app(self, class_object_list):
+        AppLogger.logger.info("creating app")
+        flask_app = Flask(__name__)
+        api = Api(flask_app)
+        for class_object in class_object_list:
+            class_object.add_routes(api)
+
+        return flask_app
+
+    @staticmethod
+    def create_app_instance(class_object_list):
+        AppLogger.logger.info("Create App Instance")
+        return App().create_app(class_object_list)
 
 
 if __name__ == '__main__':
